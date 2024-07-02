0135cda1b0f44a6059099f4d526b2303f8a1fcee
Hauk Storjord <hauks@egde.no>
2024-07-02 09:35:14 +0200

    First commit

--- src/app/main.py
+++ src/app/main.py
@@ -10,25 +10,53 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods="GET",
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-class Album():
-    def __init__(self, id, title, artist, price, image_url):
-         self.id = id
-         self.title = title
-         self.artist = artist
-         self.price = price
-         self.image_url = image_url
 
-albums = [ 
-    Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
-    Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
+class Album:
+    def __init__(self, id, title, artist, price, image_url):
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.price = price
+        self.image_url = image_url
+
+
+albums = [
+    Album(
+        1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"
+    ),
+    Album(
+        2,
+        "Seven Revision Army",
+        "The Blue-Green Stripes",
+        13.99,
+        "https://aka.ms/albums-containerappslogo",
+    ),
     Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
-    Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
-    Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-    Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+    Album(
+        4, "Lost in Translation", "MegaDNS", 12.99, "https://aka.ms/albums-envoylogo"
+    ),
+    Album(
+        5,
+        "Lock Down Your Love",
+        "V is for VNET",
+        12.99,
+        "https://aka.ms/albums-vnetlogo",
+    ),
+    Album(
+        6,
+        "Sweet Container O' Mine",
+        "Guns N Probeses",
+        14.99,
+        "https://aka.ms/albums-containerappslogo",
+    ),
+    Album(
+        7, "Notin forks", "Commitas", 20.49, "https://aka.ms/albums-containerappslogo"
+    ),
 ]
+
 
 @app.get("/")
 def read_root():
